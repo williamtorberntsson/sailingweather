7a2a47623116b25a3f8ba6c7c02e19133e6b5034
wilto837 <wilto837@student.liu.se>
2023-08-05 23:00:31 +0200

    Added weather data can be swapped between multiple locations

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import WeatherWidget from "./components/WeatherWidget";
 import FetchData from "./functions/FetchData";
 import * as myConstants from "./constants";
@@ -6,30 +6,42 @@ import "./assets/css/App.css";
 
 
 function App() {
+    // List of available locations to swap between
+    const locationList = [myConstants.ROXEN, myConstants.RENGEN];
+    let locationIndex = useRef(0);
 
     const [smhiData, setSmhiData] = useState();
+    const [location, setLocation] = useState(locationList[locationIndex.current]);
+
 
     useEffect(() => {
-        updateSMHIdata()
-    }, []);
+        updateSMHIdata(location[1]);
+    }, [location]);
 
-    const updateSMHIdata = () => {
-        FetchData(myConstants.ROXEN)
+    const updateSMHIdata = (url) => {
+        console.log("Fetch data!");
+        FetchData(url)
             .then(json => {
                 setSmhiData(json);
             })
     }
 
+    const switchLocation = () => {
+        locationIndex.current = (locationIndex.current + 1) % locationList.length;
+        setLocation(locationList[locationIndex.current]);
+    }
+
     return (
         <div className="App">
             <a className="GitHub" target="_blank" rel="noopener noreferrer" href="https://github.com/williamtorberntsson/sailingweather">Visit my GitHub to contribute or give ideas!</a>
-            <h2 className="Beta">Currently in Beta!</h2>
-            <button className="UpdateButton" onClick={updateSMHIdata}>Klicka för att uppdatera!</button>
-            <h1 className="Title">Vädret på Roxen!</h1>
+            <div className="Buttons">
+                <button className="UpdateButton" onClick={() => updateSMHIdata(location[1])}>Klicka för att uppdatera!</button>
+                <button className="UpdateButton SwitchLocation" onClick={switchLocation}>Byt position</button>
+            </div>
+            <h1 className="Title">Vädret på {location[0]}!</h1>
             <WeatherWidget data={smhiData} daysAheadToPredict={3} />
         </div>
     );
 }
 
 export default App;
-  
--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,4 @@
-export const SOMEADDRESS = "https://opendata-download-metobs.smhi.se/api/version/latest/parameter/4.json"
-export const ROXEN = "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/15.602409/lat/58.462453/data.json"
-export const NORRKOPING = "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/16.158/lat/58.5812/data.json"
-export const EXAMPLE = "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/16/lat/58/data.json"
+export const ROXEN = ["Roxen", "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/15.602409/lat/58.462453/data.json"];
+export const RENGEN = ["Rängen", "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/15.715760/lat/58.261181/data.json"];
 
 export const WeekDays = ["Söndag", "Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Lördag"];
